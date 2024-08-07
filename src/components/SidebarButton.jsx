647de970299e3e32086f603cf80a65de875c56aa
Jeong Lee <jeeonglee.dev@gmail.com>
2024-08-07 15:25:50 -0300

    feat: add Sidebar and SidebarButton component

---
+++ src/components/SidebarButton.jsx
@@ -0,0 +1,17 @@
+export function SidebarButton({ children, variant }) {
+  // Criar variações do botão
+  const getVariantClasses = () => {
+    if (variant === "unselected") {
+      return "text-[#35383e]";
+    }
+    if (variant === "selected") {
+      return "bg-[#e6f7f8] text-[#00adb5]";
+    }
+  };
+
+  return (
+    <a href="#" className={`px-6 py-3 ${getVariantClasses()}`}>
+      {children}
+    </a>
+  );
+}
